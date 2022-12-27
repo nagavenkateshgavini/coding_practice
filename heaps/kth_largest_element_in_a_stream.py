d1c0f177d8faa610f84316f2edcb4108493d8354
Naga Venkatesh Gavini <nagavenkat9948@gmail.com>
2022-12-27 18:16:36 -0500

    heap: find kth largest element of a stream of numbers

---
+++ heaps/kth_largest_element_in_a_stream.py
@@ -0,0 +1,29 @@
+from typing import List
+import heapq
+
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = nums
+        heapq.heapify(self.nums)
+
+        while len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.nums, val)
+        if len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+        
+        return self.nums[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
+if __name__ == "__main__":
+    k = KthLargest(3, [4, 5, 8, 2])
+    print(k.add(3))
