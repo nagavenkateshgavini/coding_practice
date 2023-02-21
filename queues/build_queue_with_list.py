e437eeb3b8133b95bf486872b21cc1ed7c8da09b
Naga Venkatesh Gavini <nagavenkat9948@gmail.com>
2023-02-20 19:34:38 -0500

    queue implementation with list

---
+++ queues/build_queue_with_list.py
@@ -0,0 +1,49 @@
+class queue:
+    def __init__(self):
+        self.elements = []
+        self.front = None
+        self.end = None
+        
+    def enqueue(self, num: "int"):
+        print("enqueue: ", num)
+        if not self.elements:
+            self.elements.append(num)
+            self.front = 0
+            self.end = 0
+        else:
+            self.elements.append(num)
+            self.end += 1
+    
+    def dequeue(self):
+        print("dequeu")
+        self.front += 1
+    
+    def top(self):
+        return self.elements[self.front]
+    
+    def is_empty(self):
+        return not bool(self.elements)
+    
+    def print_queue(self):
+        start = self.front
+        while start <= self.end:
+            print(self.elements[start], end=" ")
+            start += 1
+        print("")
+            
+
+def main():
+    q = queue()
+    q.enqueue(1)
+    q.enqueue(2)
+    
+    q.print_queue()
+    print("queue top", q.top())
+    q.dequeue()
+    q.enqueue(3)
+    q.print_queue()
+    print("is_empty: ", q.is_empty())
+    
+    
+if __name__ == "__main__":
+    main()
