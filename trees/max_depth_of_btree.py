f704a211a85dc371612105d1f0e42ebcd5f78110
nagavenkateshgavini <nagavenkat9948@gmail.com>
2023-05-07 20:14:28 -0400

    max depth

--- trees/max_depth_of_btree.py
+++ trees/max_depth_of_btree.py
@@ -1,3 +1,6 @@
+from typing import Optional
+from collections import deque
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -6,20 +9,54 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode], res) -> int:
-        pass
+
+    def _maxDepthRecursive(self, root: Optional[TreeNode]):
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+    def _maxDepthBFSIterative(self, root: Optional[TreeNode]):
+        q = deque()
+        q.append(root) # or q = deque([root])
+        level = 0
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.right:
+                    q.append(node.right)
+                if node.left:
+                    q.append(node.left)
+            level += 1
+        return level
+
+    def _maxDeothDFSIterative(self, root: Optional[TreeNode]):
+        stack = deque()
+        depth = 1
+        stack.append([root, depth])
+        res = 1
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        
+        return res
+
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        return self._maxDeothDFSIterative(root)
         
 
 if __name__ == "__main__":
-    t = TreeNode(4)
-    t.left = TreeNode(2)
-    t.right = TreeNode(7)
-    t.left.left = TreeNode(1)
-    t.left.right = TreeNode(3)
+    t = TreeNode(3)
+    t.left = TreeNode(9)
+    t.right = TreeNode(20)
     
-    t.right.right = TreeNode(9)
-    t.right.left = TreeNode(6)
+    t.right.right = TreeNode(7)
+    t.right.left = TreeNode(15)
+
     s = Solution()
-    res = []
-    s.maxDepth(root, res)
-    print(f"max depth: {res[0]}")
+
+    res = s.maxDepth(t)
+
+    print(f"max depth: {res}")
